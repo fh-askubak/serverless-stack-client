268d4557c0864f38b396e73126e1bba1e41f9f72
aj <askubak@fischerhomes.com>
2020-10-06 10:31:13 -0400

    geez package.json

---
+++ src/components/Encryption.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+export default () => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = event => {
+    const { value } = event.target;
+    alert('encrypt the text: ', value);
+  }
+  return (
+    <>
+    <form>
+      <FormGroup controlId="encrypt" bsSize="large">
+        <ControlLabel>Encrypt my Text!</ControlLabel>
+        <FormControl
+          autoFocus
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </FormGroup>
+      <Button
+        type="submit"
+        block
+        bsSize="large"
+      >
+        Encrypt
+      </Button>
+    </form>
+    </>
+  )
+}
